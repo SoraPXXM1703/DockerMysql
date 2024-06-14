3154d0f6568760b9cfe1abf6d00e0b11c63f3f52
SoraPXXM1703 <n0804706532@gmail.com>
2024-06-14 16:09:59 +0700

    4:10 6/14/2024

--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
 from re import escape
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, request
 import mysql.connector
 import os
 from flask import Flask
 from flask import Flask, jsonify
 import mysql.connector
-from collections import defaultdict
+from collections import OrderedDict, defaultdict
 
 app = Flask(__name__)
 
@@ -14,18 +14,20 @@ mydb = mysql.connector.connect(
   host="db",
   user="root",
   password="12345",
-  database="books_system"
+  database="books_system",
+  charset='utf8mb4',
+  collation='utf8mb4_unicode_ci'
 )
 
 mycursor = mydb.cursor()
 #jion
-@app.route('/data/buy/book')
-def get_buy_book():
-    mycursor.execute("SELECT * FROM b_books JOIN b_listpay ON b_books.Barcode = b_listpay.Barcode")
+@app.route('/data/namebook')
+def get_buy_book():#SELECT * FROM b_books JOIN b_listpay ON b_books.Barcode = b_listpay.Barcode
+    mycursor.execute("SELECT * FROM b_books JOIN b_listpay ON b_listpay.Barcode = b_books.Barcode")
     results = mycursor.fetchall()
     b_name_dict = defaultdict(list)
     for row in results:
-        b_name_dict[row[7]].append(row[3] +' '+ row[4])
+        b_name_dict[row[7]].append((row[3] + ' --> ' + row[4]))
     return jsonify(b_name_dict)
 
 
@@ -36,7 +38,7 @@ def get_book_barcode(barcode):
     b_barcode_dict = defaultdict(list)
     for row in resulits:
         if row[7] == escape(barcode):
-            b_barcode_dict[row[7]].append(row[3] + row[4]) 
+            b_barcode_dict[row[7]].append(row[3]) 
     return jsonify(b_barcode_dict)    
 
 
@@ -92,7 +94,7 @@ def get_data_name(book):
 
 
 #test 
-@app.route('/')
+@app.route('/helo')
 def name():
     return "YORNNAME....>>"
 
@@ -100,6 +102,17 @@ def name():
 @app.route('/<name>')
 def hello(name):
     return f"WELLOME > {escape(name)}"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+""" @app.route('/test/b', methods=['POST'])
+def button_clicked():
+    # ทำสิ่งที่ต้องการเมื่อปุ่มถูกคลิก
+    return 'ปุ่มถูกคลิกแล้ว!' """
+
 
 """ 
 if __name__ == '__main__':
